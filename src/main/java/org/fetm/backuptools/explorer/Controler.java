78ff5f0dea4b8ba529927b7cad75586d77002a9c
richard <richard.breguet@gmail.com>
2014-05-21 17:08:32 +0200

    mes modifications

--- src/main/java/org/fetm/backuptools/explorer/Controler.java
+++ src/main/java/org/fetm/backuptools/explorer/Controler.java
@@ -4,17 +4,45 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-/**
- * Created by Admin on 17.05.14.
- */
+/******************************************************************************
+ * Copyright (c) 2014. Richard Breguet <richard.breguet@gmail.com>            *
+ *                                                                            *
+ * This file is part of backuptools.                                          *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * any later version.                                                         *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
+ * General Public License for more details. You should have received a        *
+ * copy of the GNU General Public License along with this program.            *
+ * If not, see <http://www.gnu.org/licenses/>.                                *
+ ******************************************************************************/
+
 public class Controler {
     @FXML
     Button btAdd;
     @FXML
     TextField vaultName;
+    @FXML
+    TextField vaultPass;
+    @FXML
+    TextField vaultHost;
+    @FXML
+    TextField vaultUser;
+    @FXML
+    TextField vaultDirectory;
+
 
     @FXML
     public void onClick(){
+        String name = vaultName.getText();
+        String host = vaultHost.getText();
+        String pass = vaultPass.getText();
+        String directory = vaultDirectory.getText();
         vaultName.setText("Hello");
     }
 
--- src/main/java/org/fetm/backuptools/explorer/Main.java
+++ src/main/java/org/fetm/backuptools/explorer/Main.java
@@ -8,10 +8,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.fetm.backuptools.common.BackupAgent;
 
-/**
- * Created by Admin on 17.05.14.
- */
+/******************************************************************************
+ * Copyright (c) 2014. Richard Breguet <richard.breguet@gmail.com>            *
+ *                                                                            *
+ * This file is part of backuptools.                                          *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * any later version.                                                         *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful, but        *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU           *
+ * General Public License for more details. You should have received a        *
+ * copy of the GNU General Public License along with this program.            *
+ * If not, see <http://www.gnu.org/licenses/>.                                *
+ ******************************************************************************/
+
 public class Main extends Application{
 
     @Override
@@ -20,6 +36,8 @@ public class Main extends Application{
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+
+        BackupAgent backupAgent;
     }
 
     public static void main(String[] args){
